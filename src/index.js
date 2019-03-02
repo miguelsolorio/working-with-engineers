5a2fb3dcd99511b2f336c33f4a977e2df973ed64
Miguel Solorio <miguel.solorio@microsoft.com>
2019-03-01 21:50:42 -0800

    Meh

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import Layout from './layout'
 import BGImage from './bg-image'
 import ImageLG from './ImageLG'
 import ImageXL from './ImageXL'
-import Layout from './layout'
 import theme from './theme'
 
 export { Layout, BGImage, ImageLG, ImageXL }
