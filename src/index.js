f74ceb4ec2fdbee6f78ac9b71ae1065032479564
Miguel Solorio <miguel.solorio@microsoft.com>
2019-03-01 21:35:01 -0800

    Goodbye, Inverted

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import Layout from './layout'
-import Inverted from './Inverted'
 import BgImage from './BgImage'
 import ImageLG from './ImageLG'
 import ImageXL from './ImageXL'
 import theme from './theme'
 
-export { Layout, Inverted, BgImage, ImageLG, ImageXL }
+export { Layout, BgImage, ImageLG, ImageXL }
 
 export default theme
