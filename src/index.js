988d5c448eac259598d3bb9bd7f5566e3f97296b
Miguel Solorio <miguel.solorio@microsoft.com>
2019-03-01 20:31:26 -0800

    Remove inverted

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import Layout from './Layout'
-import Inverted from './Inverted'
 import BGImage from './Bg'
 import ImageLG from './ImageLG'
 import ImageXL from './ImageXL'
 import Theme from './Theme'
 
-export { Layout, Inverted, BGImage, ImageLG, ImageXL }
+export { Layout, BGImage, ImageLG, ImageXL }
 
 export default Theme
