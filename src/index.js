6b844239eb41682226e93e71d811a16730b54ad5
Miguel Solorio <miguel.solorio@microsoft.com>
2019-03-01 21:22:17 -0800

    Update names

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Layout from './layout'
-import BGImage from './bgImage'
-import ImageLG from './imageLG'
-import ImageXL from './imageXL'
+import BGImage from './BgImage'
+import ImageLG from './ImageLG'
+import ImageXL from './ImageXL'
 import theme from './theme'
 
 export { Layout, BGImage, ImageLG, ImageXL }
